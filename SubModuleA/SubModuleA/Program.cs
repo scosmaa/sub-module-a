c50a9a70d84c73ef91b4f17c6315a5ab09ec2898
Simone Cosma <scosmaa@gmail.com>
2019-12-19 16:27:57 +0100

    First working commit

--- SubModuleA/SubModuleA/Program.cs
+++ SubModuleA/SubModuleA/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MasterModulerSolution;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,8 @@ namespace SubModuleA
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Common.Test());
+            Console.ReadKey();
         }
     }
 }
